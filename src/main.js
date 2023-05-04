c6056dee2e47680ff8a21019ac0b59e69192b120
Daniil <eo18622@yandex.ru>
2023-05-04 16:17:41 +0300

    презентер

--- src/main.js
+++ src/main.js
@@ -12,14 +12,20 @@
 //   sum(2, 1)
 // );
 import NewFilterView from './view/new-filter-view';
-import { render } from './render';
 import NewSortView from './view/new-sort-view';
+import BoardPresenter from './presenter/board-presenter';
+import { render } from './render';
 
+// Хедер
 const siteHeaderElement = document.querySelector('.page-header');
 const siteHeaderSortElement = siteHeaderElement.querySelector('.trip-controls__filters');
 
+// Мейн
 const siteMainElement = document.querySelector('.page-main');
 const siteMainFilterWrapper = siteMainElement.querySelector('.trip-events');
+const boardPresenter = new BoardPresenter();
 
 render(new NewSortView(), siteHeaderSortElement);
 render(new NewFilterView(), siteMainFilterWrapper);
+
+boardPresenter.init();
---
+++ src/presenter/board-presenter.js
@@ -0,0 +1,19 @@
+// import NewFormAddView from '../view/new-form-add-view.js';
+import NewFormEditView from '../view/new-form-edit-view.js';
+import NewTripItemView from '../view/new-trip-item-view.js';
+import NewTripListView from '../view/new-trip-list-view.js';
+import {render} from '../render.js';
+
+export default class BoardPresenter {
+  tripListComponent = new NewTripListView();
+
+  init() {
+    render(new NewFormEditView(), this.tripListComponent.getElement());
+
+    for (let i = 0; i < 3; i++) {
+      render(new NewTripItemView(), this.tripListComponent.getElement());
+    }
+  }
+
+}
+
