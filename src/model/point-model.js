2f518795eac9f07929b4355b65f95faaf49ae459
Daniil <eo18622@yandex.ru>
2023-06-14 17:29:18 +0300

    кол-во точек

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -1,6 +1,6 @@
 import { getRandomPoint } from '../mock/point.js';
 
-const POINT_COUNT = 4;
+const POINT_COUNT = 3;
 
 export default class PointsModel {
   points = Array.from({length: POINT_COUNT}, getRandomPoint);
