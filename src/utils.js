f0d78ad017e51f32266bddabdaa65f134a2483de
Daniil <eo18622@yandex.ru>
2023-06-15 14:43:18 +0300

    комментарий

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,23 @@
 import dayjs from 'dayjs';
 
 const DATE_FORMAT = 'MMM D';
-const TIME_FORMAT = 'hh mm';
+const TIME_FORMAT = 'hh:mm';
 
 function getRandomArrayElement(items) {
   return items[Math.floor(Math.random() * items.length)];
 }
 
+// Функция для получения даты в формате: "JUL 10"
 function humanizePointDueDate(dueDate) {
   return dueDate ? dayjs(dueDate).format(DATE_FORMAT) : '';
 }
 
+// Функция для получения даты: в формате "02:55"
 function humanizeTimeDueDate(dueDate) {
   return dueDate ? dayjs(dueDate).format(TIME_FORMAT) : '';
 }
 
+// Функция для получения разницы между датой до/после
 function getDiffData(dueDate1, dueDate2) {
   const firstDate = dayjs(dueDate1);
   const secondDate = dayjs(dueDate2);
