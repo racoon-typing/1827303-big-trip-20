05fb3f73e83e943fd62736740ff1d5f553dd38c5
Daniil <eo18622@yandex.ru>
2023-06-16 16:46:46 +0300

    комментрирует

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,29 +1,29 @@
-import NewFormEditView from '../views/new-form-edit-view.js';
-import NewTripItemView from '../views/new-trip-item-view.js';
-import NewTripListView from '../views/new-trip-list-view.js';
-import NewFilterView from '../views/new-filter-view.js';
-import {render} from '../render.js';
+// import NewFormEditView from '../views/new-form-edit-view.js';
+// import NewTripItemView from '../views/new-trip-item-view.js';
+// import NewTripListView from '../views/new-trip-list-view.js';
+// import NewFilterView from '../views/new-filter-view.js';
+// import {render} from '../render.js';
 
-export default class BoardPresenter {
-  tripListComponent = new NewTripListView();
+// export default class BoardPresenter {
+//   tripListComponent = new NewTripListView();
 
-  constructor({boardContainer, pointsModel}) {
-    this.boardContainer = boardContainer;
-    this.pointsModel = pointsModel;
-  }
+//   constructor({boardContainer, pointsModel}) {
+//     this.boardContainer = boardContainer;
+//     this.pointsModel = pointsModel;
+//   }
 
-  init() {
-    this.boardPoints = [...this.pointsModel.getPoints()];
-    // console.log(this.boardPoints);
+//   init() {
+//     this.boardPoints = [...this.pointsModel.getPoints()];
+//     // console.log(this.boardPoints);
 
-    render(new NewFilterView(), this.boardContainer);
-    render(this.tripListComponent, this.boardContainer);
-    render(new NewFormEditView(), this.tripListComponent.getElement());
+//     render(new NewFilterView(), this.boardContainer);
+//     render(this.tripListComponent, this.boardContainer);
+//     render(new NewFormEditView(), this.tripListComponent.getElement());
 
-    for (let i = 0; i < this.boardPoints.length; i++) {
-      render(new NewTripItemView({item: this.boardPoints[i]}), this.tripListComponent.getElement());
-    }
-  }
+//     for (let i = 0; i < this.boardPoints.length; i++) {
+//       render(new NewTripItemView({item: this.boardPoints[i]}), this.tripListComponent.getElement());
+//     }
+//   }
 
-}
+// }
 
