468552bfb23d2b504c48f77f3d02e997b4258cc1
Daniil <eo18622@yandex.ru>
2023-06-16 11:59:12 +0300

    кард-вью

--- src/main.js
+++ src/main.js
@@ -2,32 +2,36 @@ import './views/brief-view.js';
 import './views/add-view.js';
 import './views/filter-view.js';
 import './views/sort-view.js';
+import './views/list-view.js';
 
 /**
  * @type {BriefView}
  */
 const briefView = document.querySelector('brief-view');
 
-briefView.render();
-
 /**
  * @type {AddView}
- */
+*/
 const addView = document.querySelector('add-view');
-
-addView.render();
-
 
 /**
  * @type {FilterView}
- */
+*/
 const filterView = document.querySelector('filter-view');
-
-filterView.render();
 
 /**
  * @type {SortView}
- */
+*/
 const sortView = document.querySelector('sort-view');
 
+/**
+ * @type {ListView}
+*/
+const listView = document.querySelector('list-view');
+
+
+briefView.render();
+addView.render();
+filterView.render();
 sortView.render();
+listView.render();
