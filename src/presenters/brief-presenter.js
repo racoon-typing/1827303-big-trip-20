8847d371c818315831c27e9b1a8ee82dc659f768
Daniil <eo18622@yandex.ru>
2023-09-29 13:03:16 +0300

    вывод дату в бриф презентере

--- src/presenters/brief-presenter.js
+++ src/presenters/brief-presenter.js
@@ -1,7 +1,8 @@
 import Presenter from './presenter.js';
+import { formatDateRange } from '../utils.js';
 
 /**
- * @extends {Presenter<BriefView>}
+ * @extends {Presenter<BriefView, AppModel>}
  */
 class BriefPresenter extends Presenter {
   /**
@@ -10,10 +11,40 @@ class BriefPresenter extends Presenter {
    */
   createViewState() {
     return {
-      places: 'Amsterdam — Chamonix — Geneva',
-      dates: 'Mar 18 — 20',
-      cost: '1230'
+      places: this.getPlaces(),
+      dates: this.getDates(),
+      cost: this.getCost(),
     };
+  }
+
+  /**
+   * @return {string}
+   */
+  getPlaces() {
+    return 'Amsterdam — Chamonix — Geneva';
+  }
+
+  /**
+   * @return {string}
+   */
+  getDates() {
+    const points = this.model.getPoints();
+
+    if (points.length) {
+      const firstPoint = points.at(0);
+      const lastPoint = points.at(-1);
+
+      return formatDateRange(firstPoint.startDateTime, lastPoint.endDateTime);
+    }
+
+    return '';
+  }
+
+  /**
+   * @return {string}
+   */
+  getCost() {
+    return '1230';
   }
 }
 
